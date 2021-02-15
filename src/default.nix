b31526b26a900945e0c6d1667e427d95f8ab4312
Daniel Britten <daniel.britten@outlook.com>
2021-02-15 17:30:51 +1300

    Added Coq outputs to output of nix-build

--- src/default.nix
+++ src/default.nix
@@ -10,13 +10,23 @@ pkgs.stdenv.mkDerivation {
   '';
 
   buildPhase = ''
+    make
     make --always-make Edsger/parser.ml
     make edsger
   '';
 
   installPhase = ''
+    mkdir -p $out/DeepSEA
+    cp -r backend $out/DeepSEA/backend
+    cp -r cclib $out/DeepSEA/cclib
+    cp -r core $out/DeepSEA/core
+    cp -r lib $out/DeepSEA/lib
+    cp Runtime.v $out/DeepSEA/Runtime.v
+    cp Runtime.glob $out/DeepSEA/Runtime.glob
+    cp Runtime.vo $out/DeepSEA/Runtime.vo
+
     mkdir -p $out/bin
-    cp -r _build/default/Edsger/edsger.bc $out/bin/dsc
+    cp _build/default/Edsger/edsger.bc $out/bin/dsc
     chmod +x $out/bin/dsc
   '';
 }
