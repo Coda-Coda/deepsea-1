29145285e51f04ba7ccb2bb4d41787efdf0d5446
Daniel Britten <daniel.britten@outlook.com>
2021-02-18 12:31:30 +1300

    Pinned nixpkgs in default.nix and shell.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {} } :
+let pkgs = import (builtins.fetchTarball {
+  name = "nixos-20.09-pinned";
+  url = "https://github.com/nixos/nixpkgs/archive/5e199f944cfa599636ff93e14d27e1d08ad2d9d1.tar.gz";
+  sha256 = "15z10ql82qscv3cc2l1kvqb3s9pgip1kc500szimgvzvj0fzzbll";
+}) {}; in
 pkgs.stdenv.mkDerivation {
   name = "dsc-and-deepsea-coq-built";
   buildInputs = (import ./dependencies.nix);
