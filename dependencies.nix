d7e91169b7b73a556879ea463b8109473b7d4b20
Daniel Britten <daniel.britten@outlook.com>
2021-02-18 11:34:15 +1300

    Improved how nixpkgs is pinned

--- dependencies.nix
+++ dependencies.nix
@@ -1,5 +1,8 @@
-let pkgs20-09PinnedUrl = https://github.com/nixos/nixpkgs/archive/5e199f944cfa599636ff93e14d27e1d08ad2d9d1.tar.gz; in
-let pkgs = import (fetchTarball pkgs20-09PinnedUrl) {}; in
+let pkgs = import (builtins.fetchTarball {
+  name = "nixos-20.09-pinned";
+  url = "https://github.com/nixos/nixpkgs/archive/5e199f944cfa599636ff93e14d27e1d08ad2d9d1.tar.gz";
+  sha256 = "15z10ql82qscv3cc2l1kvqb3s9pgip1kc500szimgvzvj0fzzbll";
+}) {}; in
 let
   nodePackages = import ./node-packages {
     inherit pkgs;
