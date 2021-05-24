1d961857ed2794546f165762b3719fe829535216
Daniel Britten <daniel.britten@outlook.com>
2021-05-24 16:55:44 +1200

    Improvements to building fmbc example with Nix Added shell.nix and default.nix files that simply import from the root of the repo.

--- contracts/fmbc-2021-example/build.sh
+++ contracts/fmbc-2021-example/build.sh
@@ -1,10 +1,13 @@
-# Ensure you are in a nix-shell (go to the root of the repo and run nix-shell)
+# The Nix package manager is required to run this script.
 # To get Nix go to: https://nixos.org/download.html
 # Or just run: curl -L https://nixos.org/nix/install | sh
-# macOS and Linux only (might work with WSL on Windows).
-# You can also use opam (all OSes) for the dependencies instead. In that case dsc may instead be called edsger.bc when built.
-dsc contract.ds coq
-cd contract
- coqdep -f _CoqProject > .coqdeps.d
- coq_makefile -f _CoqProject -o core.make 
- make -f core.make
+# macOS and Linux only (though it might work with WSL on Windows).
+# You can also use opam (all OSes) for the dependencies instead (see src/README.md). In that case dsc may instead be called edsger.bc when built.
+nix-build
+nix-shell --run "
+    dsc contract.ds coq
+    cd contract
+    coqdep -f _CoqProject > .coqdeps.d
+    coq_makefile -f _CoqProject -o core.make 
+    make -f core.make
+"
