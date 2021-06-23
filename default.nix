51a2ee24eb2a30409c8509475f40618bd0b7baae
Daniel Britten <daniel.britten@outlook.com>
2021-06-23 15:13:35 +1200

    Tweaks to default.nix

--- default.nix
+++ default.nix
@@ -7,18 +7,16 @@ pkgs.stdenv.mkDerivation {
   name = "dsc-and-deepsea-coq-built";
   buildInputs = (import ./dependencies.nix);
 
-  src = ./.;
+  src = ./src;
 
   postPatch = ''
     patchShebangs .
   '';
 
   buildPhase = ''
-    cd src
     make
     make --always-make Edsger/parser.ml
     make edsger
-    cd ..
   '';
 
   installPhase = ''
@@ -26,15 +24,15 @@ pkgs.stdenv.mkDerivation {
 
     echo "These files are built from https://github.com/Coda-Coda/deepsea-1 which is a fork of https://github.com/certikfoundation/deepsea/" > $out/README-result.md
     
-    cp CompCert-LICENSE.txt $out/CompCert-LICENSE.txt
-    cp Selected-Files-LICENSE.txt $out/Selected-Files-License.txt
+    #cp CompCert-LICENSE.txt $out/CompCert-LICENSE.txt
+    #cp Selected-Files-LICENSE.txt $out/Selected-Files-License.txt
 
-    cp dependencies.nix $out/dependencies.nix
+    #cp dependencies.nix $out/dependencies.nix
     
-    cp -r src/ $out/src/
+    #cp -r src/ $out/src/
 
     mkdir -p $out/binaries/built-from-src
-    cp src/_build/default/Edsger/edsger.bc $out/binaries/built-from-src/dsc
+    cp ./_build/default/Edsger/edsger.bc $out/binaries/built-from-src/dsc
     chmod +x $out/binaries/built-from-src/dsc
   '';
 }
