1c84281c3aa34a0ef7535e3de9045b74e29c62e4
Daniel Britten <daniel.britten@outlook.com>
2021-10-12 11:48:52 +1300

    Improved CEIP checker to prevent balance reads after potential reentrancy

--- core/Syntax.v
+++ core/Syntax.v
@@ -539,6 +539,28 @@ End CONSTR_PRF.
 Section CEIP_PRF.
   Context `{HM : HyperMem}.
 
+Fixpoint contains_balance_read
+    `{HyperTypeImpl}
+    (e : expr_constr tp) {struct e} : bool :=
+    match e with
+  | ECconst_int _ _ _ _ => false
+  | ECconst_int256 _ _ _ _ => false
+  | ECtempvar _ _ _ => false
+  | ECbuiltin0 _ _ possibly_balance_read =>
+    match (@Hbuiltin0 LayerSpec _ possibly_balance_read) with
+      | Bselfbalance => true
+      | _ => false
+    end
+  | ECbuiltin1 _ _ _ _ possibly_balance_read e' => 
+    match (@Hbuiltin1 LayerSpec _ _ possibly_balance_read) with
+      | Bbalance => true
+      | _ => false
+    end
+    || contains_balance_read e'
+  | ECunop _ _ _ _ _ _ e' => contains_balance_read e'
+  | ECbinop _ _ _ _ _ _ _ _ el er =>
+    (contains_balance_read el) || (contains_balance_read er)
+  end%type.
 
 Inductive cmd_constr_CEI_pattern_prf :
 
@@ -571,10 +593,12 @@ Inductive cmd_constr_CEI_pattern_prf :
     forall `{ht : HyperType tp} e,
       cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy (CCload e) Safe_no_reentrancy
       (* "load" can only be run safely from Safe_no_reentrancy, and leaves it rst as Safe_no_reentrancy *)
+      (* Note that e might contain a balance read, but there is no need to check in addition because load is already only safe in the same situation as a balance read is. *)
 | CCCEIPstore :
     forall `{ht : HyperType tp} el er,
       cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy (CCstore el er) Safe_no_reentrancy
       (* "store" can only be run safely from Safe_no_reentrancy, and leaves it rst as Safe_no_reentrancy *)
+      (* Note that e might contain a balance read, but there is no need to check in addition because store is already only safe in the same situation as a balance read is. *)
 | CCCEIPsequence :
     forall {rst1} {rst2} {rst3} r (c1 : cmd_constr void_unit_pair) (c2 : cmd_constr r),
       cmd_constr_CEI_pattern_prf void_unit_pair rst1 c1 rst2 ->
@@ -589,7 +613,7 @@ Inductive cmd_constr_CEI_pattern_prf :
   forall r e c_true c_false,
     cmd_constr_CEI_pattern_prf r Safe_no_reentrancy c_true Safe_no_reentrancy ->
     cmd_constr_CEI_pattern_prf r Safe_no_reentrancy c_false Safe_no_reentrancy ->
-    cmd_constr_CEI_pattern_prf r Safe_no_reentrancy (CCifthenelse e c_true c_false) Safe_no_reentrancy      
+    cmd_constr_CEI_pattern_prf r Safe_no_reentrancy (CCifthenelse e c_true c_false) Safe_no_reentrancy
 | CCCEIPifthenelse2 : (* Safe_no_reentrancy Safe_with_potential_reentrancy Safe_no_reentrancy *)
   forall r e c_true c_false,
     cmd_constr_CEI_pattern_prf r Safe_no_reentrancy c_true Safe_with_potential_reentrancy ->
@@ -607,35 +631,70 @@ Inductive cmd_constr_CEI_pattern_prf :
     cmd_constr_CEI_pattern_prf r Safe_no_reentrancy (CCifthenelse e c_true c_false) Safe_with_potential_reentrancy      
 | CCCEIPifthenelse5 : (* Safe_with_potential_reentrancy Safe_with_potential_reentrancy Safe_with_potential_reentrancy *)
   forall r e c_true c_false,
+    contains_balance_read e = false ->
     cmd_constr_CEI_pattern_prf r Safe_with_potential_reentrancy c_true Safe_with_potential_reentrancy ->
     cmd_constr_CEI_pattern_prf r Safe_with_potential_reentrancy c_false Safe_with_potential_reentrancy ->
     cmd_constr_CEI_pattern_prf r Safe_with_potential_reentrancy (CCifthenelse e c_true c_false) Safe_with_potential_reentrancy      
-
-| CCCEIPfor :
-    forall {rst} id_it id_end e1 e2 c,
-      cmd_constr_CEI_pattern_prf _ rst c rst ->
-      cmd_constr_CEI_pattern_prf _ rst (CCfor id_it id_end e1 e2 c) rst
-      (* This is slightly overly restrictive, for example a for loop that actually only loops once and contains a transferEth call will not be allowed. *)
-    (* "for" probably loops through c many times, so to be safe we assume it can only be run safely if c is a command which results in rst (and starts from rst). Then the loop as a whole is only safe if started from rst and will result in a rst state. *)
-| CCCEIPfirst :
-    forall {rst} r id_it id_end id_dest e1 e2 c3 c4 c5,
-      cmd_constr_CEI_pattern_prf int_bool_pair rst c3 rst ->
-      cmd_constr_CEI_pattern_prf r rst c4 rst ->
-      cmd_constr_CEI_pattern_prf r rst c5 rst ->
-      cmd_constr_CEI_pattern_prf r rst
+| CCCEIPfor1 :
+forall id_it id_end e1 e2 c,
+  contains_balance_read e1 || contains_balance_read e2 = true ->
+  cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy c Safe_no_reentrancy ->
+  cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy (CCfor id_it id_end e1 e2 c) Safe_no_reentrancy
+  (* This is slightly overly restrictive, for example a for loop that actually only loops once and contains a transferEth call will not be allowed. *)
+(* "for" probably loops through c many times, so to be safe we assume it can only be run safely if c is a command which results in rst (and starts from rst). Then the loop as a whole is only safe if started from rst and will result in a rst state. *)
+| CCCEIPfor2 :
+forall {rst} id_it id_end e1 e2 c,
+  contains_balance_read e1 = false ->
+  contains_balance_read e2 = false ->
+  cmd_constr_CEI_pattern_prf _ rst c rst ->
+  cmd_constr_CEI_pattern_prf _ rst (CCfor id_it id_end e1 e2 c) rst
+  (* This is slightly overly restrictive, for example a for loop that actually only loops once and contains a transferEth call will not be allowed. *)
+(* "for" probably loops through c many times, so to be safe we assume it can only be run safely if c is a command which results in rst (and starts from rst). Then the loop as a whole is only safe if started from rst and will result in a rst state. *)
+| CCCEIPfirst1 :
+    forall r id_it id_end id_dest e1 e2 c3 c4 c5,
+      contains_balance_read e1 || contains_balance_read e2 = true ->
+      cmd_constr_CEI_pattern_prf int_bool_pair Safe_no_reentrancy c3 Safe_no_reentrancy ->
+      cmd_constr_CEI_pattern_prf r Safe_no_reentrancy c4 Safe_no_reentrancy ->
+      cmd_constr_CEI_pattern_prf r Safe_no_reentrancy c5 Safe_no_reentrancy ->
+      cmd_constr_CEI_pattern_prf r Safe_no_reentrancy
                     (CCfirst id_it id_end id_dest e1 e2 c3 c4 c5)
-                    rst
+                    Safe_no_reentrancy
             (* Slightly over-restrictive, like for. Also the command that executes when the loop exits could move rst from Safe_no_reentrancy to Safe_with_potential_reentrancy, but this, being overly restrictive would not allow that (fairly unlikely) situation. *)
       (* With "first", similarly to "for" we assume all the commands (which may be looped) result in rst (and start from rst). So the overall first-loop is only safe if started from rst and will result in a rst state. *)
-| CCCEIPfold :
-    forall `{ht : HyperType tp} {rst} id_it id_end id_recur id_dest e1 e2 e3 c,
-      cmd_constr_CEI_pattern_prf _ rst c rst ->
-      cmd_constr_CEI_pattern_prf _
-        rst
-        (CCfold id_it id_end id_recur id_dest e1 e2 e3 c)
-        rst
-        (* Similarly to CCCEIPfor, this is overly restrictive. *)
-    (* With "fold", similarly to "for" and "first" we assume all the commands (which may be looped) result in rst (and start from rst). So the overall fold is only safe if started from rst and will result in a rst state. *)
+| CCCEIPfirst2 :
+forall {rst} r id_it id_end id_dest e1 e2 c3 c4 c5,
+  contains_balance_read e1 = false ->
+  contains_balance_read e2 = false ->
+  cmd_constr_CEI_pattern_prf int_bool_pair rst c3 rst ->
+  cmd_constr_CEI_pattern_prf r rst c4 rst ->
+  cmd_constr_CEI_pattern_prf r rst c5 rst ->
+  cmd_constr_CEI_pattern_prf r rst
+                (CCfirst id_it id_end id_dest e1 e2 c3 c4 c5)
+                rst
+        (* Slightly over-restrictive, like for. Also the command that executes when the loop exits could move rst from Safe_no_reentrancy to Safe_with_potential_reentrancy, but this, being overly restrictive would not allow that (fairly unlikely) situation. *)
+  (* With "first", similarly to "for" we assume all the commands (which may be looped) result in rst (and start from rst). So the overall first-loop is only safe if started from rst and will result in a rst state. *)
+| CCCEIPfold1 :
+  forall `{ht : HyperType tp} id_it id_end id_recur id_dest e1 e2 e3 c,
+    contains_balance_read e1 || contains_balance_read e2 || contains_balance_read e3 = true ->
+    cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy c Safe_no_reentrancy ->
+    cmd_constr_CEI_pattern_prf _
+      Safe_no_reentrancy
+      (CCfold id_it id_end id_recur id_dest e1 e2 e3 c)
+      Safe_no_reentrancy
+      (* Similarly to CCCEIPfor, this is overly restrictive. *)
+  (* With "fold", similarly to "for" and "first" we assume all the commands (which may be looped) result in rst (and start from rst). So the overall fold is only safe if started from rst and will result in a rst state. *)
+| CCCEIPfold2 :
+  forall `{ht : HyperType tp} {rst} id_it id_end id_recur id_dest e1 e2 e3 c,
+    contains_balance_read e1 = false ->
+    contains_balance_read e2 = false ->
+    contains_balance_read e3 = false ->
+    cmd_constr_CEI_pattern_prf _ rst c rst ->
+    cmd_constr_CEI_pattern_prf _
+      rst
+      (CCfold id_it id_end id_recur id_dest e1 e2 e3 c)
+      rst
+      (* Similarly to CCCEIPfor, this is overly restrictive. *)
+(* With "fold", similarly to "for" and "first" we assume all the commands (which may be looped) result in rst (and start from rst). So the overall fold is only safe if started from rst and will result in a rst state. *)
 | CCCEIPcall1 :
     forall {rst1} {rst2} r argt prim arg,
       rst1 = prim.(PRIMrst_before_A argt r) -> rst2 = prim.(PRIMrst_after_A argt r) -> cmd_constr_CEI_pattern_prf r rst1 (CCcall prim arg) rst2
@@ -647,10 +706,16 @@ Inductive cmd_constr_CEI_pattern_prf :
 (* | CCCEIPcall_ext : 
         (* Note: this should be similar to CCCEIPtransfer and also to CCPcall_ext *)
 *)
-| CCCEIPyield :
-    forall {rst} `{ht : HyperType tp} e,
-      cmd_constr_CEI_pattern_prf _ rst (CCyield e) rst
-    (* "yield" retains the current rst *)
+| CCCEIPyield1 :
+  forall `{ht : HyperType tp} e,
+    contains_balance_read e = true ->
+      cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy (CCyield e) Safe_no_reentrancy
+    (* If a balance is read in the expression "yield" is only safe before potential reentrancy. *)
+| CCCEIPyield2 :
+  forall {rst} `{ht : HyperType tp} e,
+    contains_balance_read e = false ->
+    cmd_constr_CEI_pattern_prf _ rst (CCyield e) rst
+    (* "yield" retains the current rst as long as the expression does not read a balance. *)
 | CCCEIPconstr :
     forall `{ht : HyperType tp} fld_ids fld_tps el flds constr,
       cmd_constr_CEI_pattern_prf _ Safe_no_reentrancy (CCconstr fld_ids fld_tps el flds constr) Safe_no_reentrancy
