3efd21060cebf4de52102adc1ba31afcfd8d4400
Daniel Britten <daniel.britten@outlook.com>
2021-10-12 12:19:10 +1300

    Add some tests for Checks-Effects-Interaction-Pattern Note that this only tests a handful of situations where the Checks-Effects-Interactions pattern _is_ followed, not when it it is not followed. Can be used as a starting point for manaully trying out various situations where it is not followed also.

---
+++ integration-tests/checks_effects_interaction_pattern/run_tests.sh
@@ -0,0 +1,55 @@
+# TODO - improve and standardise between integration tests
+
+rm -f test_summary.log
+rm -f FunctionalCorrectness.vo
+rm -f FunctionalCorrectness.glob
+
+# Compile to Coq Test
+echo "🛈  Compiling to Coq: dsc ... coq" 1>> test_summary.log
+./compile_for_coq.sh; compilationExitCode=$?
+if [ $compilationExitCode -eq 0 ]; then
+    echo " ✅ Test Passed" 1>> test_summary.log
+else
+    echo " ❌ Test FAILED" 1>> test_summary.log
+    echo "-----SUMMARY------"
+    cat test_summary.log
+    exit 1
+fi
+
+# # Compile Functional Correcteness Test
+# coqdep -f _CoqProject > .coqdeps.d
+# coq_makefile -f _CoqProject -o core.make 
+# echo "🛈  FunctionalCorrectness.v Compilation Test" 1>> test_summary.log
+# make -f core.make; FCcompilationExitCode=$?
+# if [ $FCcompilationExitCode -eq 0 ]; then
+#     echo " ✅ Test Passed" 1>> test_summary.log
+# else
+#     echo " ❌ Test FAILED" 1>> test_summary.log
+#     echo "-----SUMMARY------"
+#     cat test_summary.log
+#     exit 1
+# fi
+
+# # No top-level axioms test
+# rm -f axioms.log
+# rm -f coqchk/FunctionalCorrectness.vo
+# mkdir -p coqchk
+# cp FunctionalCorrectness.vo coqchk/FunctionalCorrectness.vo
+# cd coqchk
+# coqchk -silent -o -norec SmartContract.FunctionalCorrectness -R ../../.. DeepSpec -R ../checks_effections_interaction_pattern checks_effections_interaction_pattern -R . SmartContract 2> ../axioms.log
+# cd ..
+# AXIOM_FILTER_STRING='    SmartContract.'
+# NUMBER_OF_AXIOMS_AT_TOP_LEVEL=$(cat axioms.log | grep -c "$AXIOM_FILTER_STRING")
+# echo "🛈  FunctionalCorrectness.v Top Level Axioms Test" 1>> test_summary.log
+# if [ $NUMBER_OF_AXIOMS_AT_TOP_LEVEL == "0" ]; then
+#         echo " ✅ Test Passed (no axioms at the top level)" 1>> test_summary.log
+#     else
+#         echo " ❌ Test FAILED ($NUMBER_OF_AXIOMS_AT_TOP_LEVEL axiom(s) at the top level present):" 1>> test_summary.log
+#         less axioms.log | grep "$AXIOM_FILTER_STRING"  1>> test_summary.log
+#         echo "-----SUMMARY------"
+#         cat test_summary.log
+#         exit 1
+# fi
+
+echo "-----SUMMARY------"
+cat test_summary.log
