a61b6cf30153986f42aca7b9fb9ae6bea3f2c8f9
Daniel Britten <daniel.britten@outlook.com>
2021-12-03 17:34:08 +1300

    Added placeholder comment to CEIP integration test And made more similar to existing tests.

--- integration-tests/checks_effects_interaction_pattern/FunctionalCorrectness.v
+++ integration-tests/checks_effects_interaction_pattern/FunctionalCorrectness.v
@@ -16,23 +16,7 @@ Import core.MemoryModel.
 Section WithMem.
 Context {memModelOps : MemoryModelOps mem}.
 
-Theorem eq_correct_one_direction : forall i u d d' me b,
-   runStateT (Contract_eq_opt i u me) d = Some (b, d') ->
-   i = Int256.unsigned u ->
-   u = Int256.repr i ->
-   b = true.
-Proof.
-  intros.
-  Transparent Contract_eq_opt. unfold Contract_eq_opt in H.
-  inv_runStateT_branching.
-  Search Int256.eq "=".
-  destruct u.
-  simpl in *. subst.
-  apply andb_true_intro.
-  split.
-  apply Z.eqb_refl.
-  rewrite H1.
-  apply Int256.eq_true.
-Qed.
+(* Could add proofs here, however the tests currently do not
+   involve FunctionalCorrectness.v *)
 
 End WithMem.
